dadff96898c28fdb05e7e2ad0567637835da030f
liveseasonal <matt@liveseasonal.com>
2016-06-21 15:34:46 +0000

    Added flash notices and alerts

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,11 +15,23 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
-  def create
+  # def create
+  #       @movie = Movie.new(movie_params)
+
+  #       if @movie.save
+  #         redirect_to movies_path
+  #       else
+  #         render :new
+  #       end
+  #     end
+
+#added code from flash
+
+   def create
         @movie = Movie.new(movie_params)
 
         if @movie.save
-          redirect_to movies_path
+          redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
         else
           render :new
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,25 @@ class UsersController < ApplicationController
   #   end
   # end
 
-   def create
+   # def create
+   #      @user = User.new(user_params)
+
+   #      if @user.save
+   #        session[:user_id] = @user.id # auto log in
+   #        redirect_to movies_path
+   #      else
+   #        render :new
+   #      end
+   #    end
+
+#Added flash code
+
+    def create
         @user = User.new(user_params)
 
         if @user.save
-          session[:user_id] = @user.id # auto log in
-          redirect_to movies_path
+          session[:user_id] = @user.id
+          redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
         else
           render :new
         end

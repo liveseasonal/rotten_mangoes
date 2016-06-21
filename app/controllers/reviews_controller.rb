ec6abe28435a54cc0c842b68c33061e3748a00d5
liveseasonal <matt@liveseasonal.com>
2016-06-21 19:14:14 +0000

    Before helpers added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,36 @@
 class ReviewsController < ApplicationController
-   def new
-        @movie = Movie.find(params[:movie_id])
+   # def new
+   #      @movie = Movie.find(params[:movie_id])
+   #      @review = @movie.reviews.build
+   #    end
+
+   #    def create
+   #      @movie = Movie.find(params[:movie_id])
+   #      @review = @movie.reviews.build(review_params)
+   #      @review.user_id = current_user.id
+
+   #      if @review.save
+   #        redirect_to @movie, notice: "Review created successfully"
+   #      else
+   #        render :new
+   #      end
+   #    end
+
+   #    protected
+
+   #    def review_params
+   #      params.require(:review).permit(:text, :rating_out_of_ten)
+   #    end
+
+   #With a little refactoring
+
+    before_filter :load_movie
+
+      def new
         @review = @movie.reviews.build
       end
 
       def create
-        @movie = Movie.find(params[:movie_id])
         @review = @movie.reviews.build(review_params)
         @review.user_id = current_user.id
 
@@ -17,6 +42,10 @@ class ReviewsController < ApplicationController
       end
 
       protected
+
+      def load_movie
+        @movie = Movie.find(params[:movie_id])
+      end
 
       def review_params
         params.require(:review).permit(:text, :rating_out_of_ten)

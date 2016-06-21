8af5e03b05e326735867d02651dc30de4b3b5fbd
liveseasonal <matt@liveseasonal.com>
2016-06-21 20:08:07 +0000

    rooted to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
 
 
 
-  get 'reviews/new'
-
-  get 'reviews/create'
+ 
 
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
   #The below refactored
 
